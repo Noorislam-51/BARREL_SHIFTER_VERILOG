0c15f7651d04191b8264dcf4e17dd6d05bc8190e
Noor Islam <noorislam5114@gmail.com>
2025-12-31 14:31:17 +0530

    fkkjal

--- barrel_shifter.v
+++ barrel_shifter.v
@@ -4,18 +4,18 @@ module barrel_shifter(
   input dir,//if 1 right shift //if 0 left shift
   input [2:0]shift //2^3=8
 );
-
-always @(*)begin
-  if(dir)
-    OUT  = IN>>shift;
-  else
-    OUT = IN<<shift;  
-end
-// rotate
-// always @(*) begin
-//   if (dir)   // rotate right
-//     OUT = (IN >> shift) | (IN << (8 - shift));
-//   else       // rotate left
-//     OUT = (IN << shift) | (IN >> (8 - shift));
+// shift 
+// always @(*)begin
+//   if(dir)
+//     OUT  = IN>>shift;
+//   else
+//     OUT = IN<<shift;  
 // end
+// rotate
+always @(*) begin
+  if (dir)   // rotate right
+    OUT = (IN >> shift) | (IN << (8 - shift));
+  else       // rotate left
+    OUT = (IN << shift) | (IN >> (8 - shift));
+end
 endmodule
